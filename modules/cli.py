3e4aeb5194c24a7c0856eb41ce04ee517e62233f
Anna <128635799+anna0range@users.noreply.github.com>
2023-10-25 17:32:56 +0000

    menu revamp

--- modules/cli.py
+++ modules/cli.py
@@ -8,16 +8,20 @@ l'interface utilisateur à la console
 def greet():
     """() -> None"""
     # TODO afficher un message d'acceuil
-    print("Insérer un message d'accueil ici")
+    print("Bienvenue à notre programme! Nous effectuerons des calculations et le dessin d'un cercle.")
+
 
 def menu(items):
-    """(list:str) -> str"""
+    """(list of str) -> str"""
     # TODO remplacer les instructions ci-dessous pour \
     # afficher correctement les instructions, le menu et \
     # pour saisir le choix
+    print("MENU")
+    print("----")
     for i in items:
-        print(i)
-    return ""
+        print(items.index(i)+1, i)
+    
+    return input("Saisissez votre choix: ")
 
 def clear():
     """() -> None"""
@@ -26,14 +30,14 @@ def clear():
 def close():
     """() -> None"""
     # TODO afficher un message de sortie
-    print("Insérer un message de sortie ici")
+    print("Merci d'avoir utilisé notre programme. Au revoir!")
 
 # Code pour tester cette module indépendamment du programme principal
 if __name__ == "__main__":  
     # TODO ajouter/modifier les tests unitaires pour les fonctions
     clear()
     greet()
-    choix = menu(["un", "deux", "trois"])
+    choix = menu(["op1", "op2", "op3"])
     print("choix =", choix)
     close()
     
