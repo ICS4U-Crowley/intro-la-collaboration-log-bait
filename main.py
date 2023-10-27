b7d64f11642d111beb85ac351189ebee17cbbe47
Gamerplex01 <128634965+Gamerplex01@users.noreply.github.com>
2023-10-27 14:01:35 -0400

    yes

--- main.py
+++ main.py
@@ -7,12 +7,15 @@ Point de lancement du programme
 """
 
 options = ["Calculatrice", "Animation", "Quitter"]
-optionsCalc = ["Addition", "Soustraction", "Multiplication", "Division", "Quitter" ]
+optionsCalc = ["Addition", "Soustraction",
+               "Multiplication", "Division", "Quitter"]
+
 
 def initialise():
     """() -> None"""
     cli.clear()
     cli.greet()
+
 
 def loop():
     """() -> None"""
@@ -36,8 +39,7 @@ def loop():
         elif choix == "3":
             return
         cli.clear()
-        
-        
+
 
 def terminate():
     """() -> None"""
