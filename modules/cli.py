8b63e9905c9e294e6497a93cc1c59679a0dc2f80
Anna <128635799+anna0range@users.noreply.github.com>
2023-10-27 17:51:06 +0000

    2

--- modules/cli.py
+++ modules/cli.py
@@ -7,15 +7,13 @@ l'interface utilisateur à la console
 
 def greet():
     """() -> None"""
-    # TODO afficher un message d'acceuil
+    # Afficher un message d'acceuil
     print("Bienvenue à notre programme! Nous effectuerons des calculations et le dessin d'un cercle.")
 
 
 def menu(items):
     """(list of str) -> str"""
-    # TODO remplacer les instructions ci-dessous pour \
-    # afficher correctement les instructions, le menu et \
-    # pour saisir le choix
+    # Afficher le menu
     print("MENU")
     print("----")
     for i in items:
@@ -29,12 +27,12 @@ def clear():
 
 def close():
     """() -> None"""
-    # TODO afficher un message de sortie
+    # Afficher un message de sortie
     print("Merci d'avoir utilisé notre programme. Au revoir!")
 
 # Code pour tester cette module indépendamment du programme principal
 if __name__ == "__main__": 
-    # TODO ajouter/modifier les tests unitaires pour les fonctions
+    # Tests unitaires
     clear()
     greet()
     choix = menu(["op1", "op2", "op3"])
